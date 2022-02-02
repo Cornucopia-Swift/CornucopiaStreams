f4c6aa32b04597fa99659d4bd8e5e3dbaabaf777
Dr. Michael Lauer <mickey@vanille-media.de>
2022-02-02 11:40:57 +0100

    BLE: The connection provider now takes two forms of URLs: 1.) ble://<service-uuid> 2.) ble://<service-uuid>/<device-uuid>
    This is necessary to specify a concrete device when you have multiple ones that
    provide the same service.
    
    Closes #3

--- Sources/CornucopiaStreams/ConnectionProviders/BLEConnectionProvider.swift
+++ Sources/CornucopiaStreams/ConnectionProviders/BLEConnectionProvider.swift
@@ -12,33 +12,33 @@ public extension Stream {
 
         static let forbiddenCharsetCBUUID4 = CharacterSet(charactersIn: "0123456789ABCDEF").inverted
         static let forbiddenCharsetCBUUID6 = CharacterSet(charactersIn: "0123456789ABCDEF-").inverted
+        static let numberOfCharactersForPeerUUID = 36
 
-        lazy var accessoryManager: BLEAccessoryManager = .init()
+        var accessoryManager: BLEAccessoryManager?
         var uuid: CBUUID!
-        weak var peripheral: CBPeripheral?
+        var peer: UUID?
 
         public override func setup() {
 
-            guard let service = self.url.host?.uppercased() else {
-                self.failWith(error: .invalidParameters)
-                return
+            guard let service = self.url.host?.uppercased() else { return self.failWith(error: .invalidParameters) }
+            if self.url.path.count == Self.numberOfCharactersForPeerUUID + 1 {
+                let uuidString = String(self.url.path.dropFirst())
+                guard let peer = UUID(uuidString: uuidString) else { return self.failWith(error: .invalidParameters) }
+                self.peer = peer
             }
+
             switch service.count {
                 case 4:
-                    guard service.rangeOfCharacter(from: Self.forbiddenCharsetCBUUID4) == nil else {
-                        return self.failWith(error: .invalidParameters)
-                    }
+                    guard service.rangeOfCharacter(from: Self.forbiddenCharsetCBUUID4) == nil else { return self.failWith(error: .invalidParameters) }
                     uuid = CBUUID(string: service)
                 case 6, 36:
-                    guard service.rangeOfCharacter(from: Self.forbiddenCharsetCBUUID6) == nil else {
-                        return self.failWith(error: .invalidParameters)
-                    }
+                    guard service.rangeOfCharacter(from: Self.forbiddenCharsetCBUUID6) == nil else { return self.failWith(error: .invalidParameters) }
                     uuid = CBUUID(string: service)
                 default:
                     return self.failWith(error: .invalidParameters)
             }
 
-            self.accessoryManager.findService(with: self.uuid) { result in
+            self.accessoryManager = BLEAccessoryManager(uuid: self.uuid, peer: self.peer) { result in
                 switch result {
                     case .success(let streams):
                         self.meta.name = streams.peripheral.name ?? ""
@@ -50,7 +50,7 @@ public extension Stream {
         }
 
         public override func cancel() {
-            self.accessoryManager.cancelFind(with: self.uuid)
+            self.accessoryManager?.cancel()
         }
     }
 }
